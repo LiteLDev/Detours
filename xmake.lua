76c05a37e40116d708ca33fb48e572c4e69a3498
RimuruChan <RealSprite233@outlook.com>
2023-03-16 08:57:02 +0800

    build: remove useless cxx flags

--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,15 @@
+add_rules("mode.debug", "mode.release")
+
+if is_mode("debug") then
+    set_runtimes("MDd")
+else
+    set_runtimes("MD")
+end
+
 target("detours")
     set_kind("static")
-    set_languages("c++11")
+    set_languages("c++17")
     add_headerfiles("detours/src/*.h", { prefixdir = "detours" })
     add_includedirs("detours/src")
     add_files("detours/src/*.cpp")
     remove_files("detours/src/uimports.cpp") -- This file is included and not compiled on its own
-    if is_mode("debug") then
-        set_optimize("none")
-        set_symbols("debug")
-        add_cxflags("/MDd", "/Zi", "/Gy", "/Gm-", "/Zl")
-    elseif is_mode("release") then
-        set_optimize("fastest")
-        set_symbols("hidden")
-        add_cxflags("/MD", "/O2", "/Gy", "/Gm-", "/Zl")
-    end
